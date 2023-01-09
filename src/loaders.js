cb812f3cf4bc437c5a48ee83f931c06907afc4df
lanbow93 <the.doctor1501@gmail.com>
2023-01-09 17:18:00 -0600

    troubleshooting

--- src/loaders.js
+++ src/loaders.js
@@ -1,14 +1,14 @@
-const URL = process.env.REACT_APP_URL
+const DB_URL = "https://express-react-app-backend.onrender.com/"
 
 export const projectLoader = async ({request}) => {
-    const response = await fetch(`${URL}projects`);
+    const response = await fetch(`${DB_URL}projects`);
     const projects = await response.json()
     return projects
 
 }
 
 export const aboutLoader = async ({request}) => {
-    const response = await fetch(`${URL}about`);
+    const response = await fetch(`${DB_URL}about`);
     console.log(response)
     const about =  await response.json()
     console.log(about)
