99abdc53e369c56700a64dac4339fb9bd352621c
lanbow93 <the.doctor1501@gmail.com>
2023-01-09 15:46:07 -0600

    Changed how url was accessed

--- src/loaders.js
+++ src/loaders.js
@@ -1,14 +1,14 @@
 const URL = process.env.REACT_APP_URL
 
 export const projectLoader = async ({request}) => {
-    const response = await fetch(URL + "/projects");
+    const response = await fetch(`${URL}/projects`);
     const projects = await response.json()
     return projects
 
 }
 
 export const aboutLoader = async ({request}) => {
-    const response = await fetch(URL + "/about");
+    const response = await fetch(`${URL}/about`);
     const about =  await response.json()
     return about
 }
